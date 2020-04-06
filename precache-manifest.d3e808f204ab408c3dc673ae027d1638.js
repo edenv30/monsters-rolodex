4c052f9e91724dc1024b768844e2bab24f179d82
eden varsulker <edenv30@gmail.com>
2020-04-07 00:26:01 +0300

    Updates

---
+++ precache-manifest.d3e808f204ab408c3dc673ae027d1638.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd771d4bfe21f064d4294629b3953d1e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "710573dbd3f855f0f6cd",
+    "url": "/monsters-rolodex/static/css/main.6c5c3ff3.chunk.css"
+  },
+  {
+    "revision": "147f1327e7798733efc4",
+    "url": "/monsters-rolodex/static/js/2.d562cfe4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.d562cfe4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "710573dbd3f855f0f6cd",
+    "url": "/monsters-rolodex/static/js/main.c502a0fe.chunk.js"
+  },
+  {
+    "revision": "83fef09b6b4b91e8e383",
+    "url": "/monsters-rolodex/static/js/runtime-main.6036d2a0.js"
+  }
+]);
